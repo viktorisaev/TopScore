da95a5cc9210df5e405e340a6aebeaf1d03ba651
Victor Isaev <victorbisaev@gmail.com>
2025-02-22 15:04:29 +0100

    refine visuals
    made teams' background 50% transparent

--- topscore.js
+++ topscore.js
@@ -4,10 +4,10 @@ setInterval(function () {
   const timestamp = new Date().getTime();
   console.log("In timer..." + timestamp);
 
-  document.getElementsByClassName("score leftpanel")[0].textContent = timestamp
+  document.getElementsByClassName("score leftteam")[0].textContent = timestamp
     .toString()
     .slice(10, 12);
-  document.getElementsByClassName("score rightpanel")[0].textContent = timestamp
+  document.getElementsByClassName("score rightteam")[0].textContent = timestamp
     .toString()
     .slice(11, 13);
 }, 978);
