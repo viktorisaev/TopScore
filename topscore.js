4609e451e1d27d6f173616f500c5d8dac22086f9
Victor Isaev <victorbisaev@gmail.com>
2025-03-13 20:54:29 +0100

    Add animation on [A] button

--- topscore.js
+++ topscore.js
@@ -2,7 +2,7 @@ console.log("Start JS code for the page");
 
 setInterval(function () {
   const timestamp = new Date().getTime();
-  console.log("In timer..." + timestamp);
+  //  console.log("In timer..." + timestamp);
 
   document.getElementsByClassName("score leftteam")[0].textContent = timestamp
     .toString()
@@ -11,3 +11,39 @@ setInterval(function () {
     .toString()
     .slice(11, 13);
 }, 978);
+
+window.addEventListener("load", function (e) {
+  console.log("Document loaded.");
+
+  el = document.querySelector(".panel");
+  el.style.visibility = "hidden";
+
+  requestAnimationFrame(function (e) {
+    console.log("in anim frame");
+
+    el = document.querySelector(".panel");
+    el.classList.add("fademein");
+  });
+
+  // EVENT: keypressed
+  addEventListener("keypress", function (event) {
+    console.log("Key pressed:", event.key);
+
+    if (event.key == "a") {
+      var animEl = this.document.getElementById("anim1");
+      if (animEl) {
+        animEl.classList.remove("brightend");
+        requestAnimationFrame(function (e) {
+          animEl.classList.add("brightend");
+        });
+      } else {
+        console.error("anim1 not found!!!");
+      }
+    }
+  });
+
+  // EVENT: animation end
+  addEventListener("animationend", function (event) {
+    //    console.log("animationend", event);
+  });
+});
